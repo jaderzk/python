17caa27c634fad97c31ed56e81e30457deb846a7
jaderzk <jader.rojas@gmail.com>
2022-03-17 17:41:05 -0500

    Uso de numpy

--- pandass.py
+++ pandass.py
@@ -1,4 +1,15 @@
 import pandas as pd
+import numpy as np
+
+arreglo = [1,21,3,4,5,6]
+nmpys = np.array(arreglo)
+
+print("el tamaño del arreglo es: ", nmpys.size)
+
+print("el maximo del arreglo es: ", nmpys.max())
+
+print("el mínimo del arreglo es: ", nmpys.min())
+
 
 print ("Ejercicio de Dataframes con Pandas")
 dffelicidad = pd.read_csv("https://raw.githubusercontent.com/AlanCIO/hapinnes/main/world-happiness-report-2021.csv")
@@ -6,3 +17,9 @@ print(dffelicidad)
 
 for d in dffelicidad:
     print("dato:", d)
+    
+
+print("Las columnas del DataFrame:", dffelicidad.columns)    
+print("Los valores del DataFrame:", dffelicidad.values) 
+    
+print("La cantidad de valores del DataFrame:", dffelicidad.count())    
